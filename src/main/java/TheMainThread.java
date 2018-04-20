2b4b8c6fcc8d91805ddb42b6df70a4b576185cf2
Nitin Mahendru <nitin@Nitins-MacBook-Pro-15.local>
2018-04-19 23:26:11 -0700

    fixed the thread hanging issue. chnage my email

--- src/main/java/TheMainThread.java
+++ src/main/java/TheMainThread.java
@@ -2,26 +2,31 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashSet;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
 
 interface VoltageWorker extends Runnable{
 
     public void shutdown();
     public void execute(VoltageWork a);
-
+    public void start();
 
 }
 class WorkerThread implements VoltageWorker {
     boolean shouldExit;
     Object lock;
     VoltageWork w;
+    Thread t;
 
     WorkerThread() {
         shouldExit = false;
         lock = new Object();
-        this.run();
+        t = new Thread(this);
     }
-
+    public void start(){
+        t.start();
+    }
     public void execute(VoltageWork in) {
        synchronized(lock) {
            w = in;
@@ -76,7 +81,7 @@ class WorkerThread implements VoltageWorker {
 }
 
 class ThreadPool{
-    PriorityBlockingQueue<VoltageWorker> threads;
+    LinkedBlockingQueue<VoltageWorker> threads;
     int numThreads;
     public ThreadPool(){
         __init(5);
@@ -85,10 +90,12 @@ class ThreadPool{
         __init(num);
     }
     private void __init(int n){
-        threads = new PriorityBlockingQueue<>();
+        threads = new LinkedBlockingQueue<>(n);
         numThreads = n;
         for(int i = 0 ; i < numThreads ; i++){
-            threads.add(new WorkerThread());
+            WorkerThread t = new WorkerThread();
+            t.start();
+            threads.add(t);
         }
     }
     public void execute(VoltageWork a) {
@@ -99,11 +106,11 @@ class ThreadPool{
                 t = threads.take();
                 t.execute(a);
                 workDone = true;
-                threads.put(t);
+                threads.offer(t);
             } catch (InterruptedException e) {
                 if (workDone) {
                     if (t != null) {
-                        threads.put(t);
+                        threads.offer(t);
                     }
                     break;
                 }
@@ -138,8 +145,14 @@ interface VoltageWork{
  * This is all the user has to implement
  * ***/
 class Work implements VoltageWork{
+    static int i = 0;
+    int instanceNumber;
+    public Work(){
+        instanceNumber = i++;
+    }
     private void doREST(){
-        System.out.println("Did a REST call ");
+        System.out.println("Did a REST call " + instanceNumber);
+
     }
 
     public void work() throws VoltageException{
